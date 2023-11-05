25a2fe7c9be2dbb1ade35385231b95030a9fb665
YOUR_NAME <YOUR_EMAIL>
2023-11-06 01:13:22 +0300

    update2

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
 from .models import Post
 
-# Register your models here.
-#admin.site.register(Post)
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
@@ -13,5 +11,3 @@ class PostAdmin(admin.ModelAdmin):
     raw_id_fields = ['author']
     date_hierarchy = 'publish'
     ordering = ['status', 'publish']
-
-
--- blog/models.py
+++ blog/models.py
@@ -5,20 +5,21 @@ from django.contrib.auth.models import User
 
 class PublishedManager(models.Manager):
     def get_queryset(self):
-        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
+        return super().get_queryset()\
+                      .filter(status=Post.Status.PUBLISHED)
 
 
 class Post(models.Model):
 
     class Status(models.TextChoices):
-        DRAFT = 'DF', 'DRAFT'
+        DRAFT = 'DF', 'Draft'
         PUBLISHED = 'PB', 'Published'
 
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
     author = models.ForeignKey(User,
                                on_delete=models.CASCADE,
-                               related_name='blog_post')
+                               related_name='blog_posts')
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
@@ -26,8 +27,9 @@ class Post(models.Model):
     status = models.CharField(max_length=2,
                               choices=Status.choices,
                               default=Status.DRAFT)
-    objects = models.Manager()
-    published = PublishedManager()
+
+    objects = models.Manager() # The default manager.
+    published = PublishedManager() # Our custom manager.
 
     class Meta:
         ordering = ['-publish']
@@ -35,7 +37,5 @@ class Post(models.Model):
             models.Index(fields=['-publish']),
         ]
 
-
     def __str__(self):
         return self.title
-
